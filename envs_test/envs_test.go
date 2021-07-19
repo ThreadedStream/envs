55416ad077cd5aaf18f68d37f513a5399c29d1c2
ThreadedStream <frankflipper3@gmail.com>
2021-07-19 13:49:47 +0300

    fixed notorious bug with incorrect parsing values containing  ":" symbol

--- envs_test/envs_test.go
+++ envs_test/envs_test.go
@@ -21,6 +21,11 @@ func TestParse(t *testing.T) {
 			UseSSL bool   `env:"USE_SSL" fallback:"false"`
 		}{}
 
+		network = struct {
+			Host     string `env:"HOST" fallback:"http://localhost:3000"`
+			UseProxy bool   `env:"USE_PROXY" fallback:"false"`
+		}{}
+
 		err error
 	)
 
@@ -35,14 +40,23 @@ func TestParse(t *testing.T) {
 	err = envs.Parse(&exported)
 	assert.Equal(t, err, nil)
 
-	err = envs.Parse(&unexported)
-	assert.Equal(t, err, nil)
-
 	assert.Equal(t, exported.Host, "0.0.0.0")
 	assert.Equal(t, exported.Port, 7000)
 	assert.Equal(t, exported.UseSSL, true)
 
+	err = envs.Parse(&unexported)
+	assert.Equal(t, err, nil)
+
 	assert.Equal(t, unexported.host, "")
 	assert.Equal(t, unexported.port, 0)
 	assert.Equal(t, unexported.UseSSL, false)
+
+	err = os.Setenv("HOST", "http://localhost:3500")
+	assert.Equal(t, err, nil)
+
+	err = envs.Parse(&network)
+	assert.Equal(t, err, nil)
+
+	assert.Equal(t, network.Host, "http://localhost:3500")
+	assert.Equal(t, network.UseProxy, false)
 }
